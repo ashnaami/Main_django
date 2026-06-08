de2dd5b3d798531e1292dd8c263a09465a592585
ashnaami <ashnahaleema@gmail.com>
2026-06-08 09:08:28 +0530

    Updated backend2

--- blogg_api/settings.py
+++ blogg_api/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "https://main-react-ashna-haleema-s-projects.vercel.app/",
+    "https://main-react-ashna-haleema-s-projects.vercel.app",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
